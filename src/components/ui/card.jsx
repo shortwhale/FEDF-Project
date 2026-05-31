668cb8f58ea652481f2f9b0397506a0610b98368
Mahitha <mahithar45@gmail.com>
2026-05-31 15:49:06 +0530

    Add files via upload

---
+++ src/components/ui/card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export function Card({ children, className = "", ...props }) {
+  return (
+    <div className={`bg-white border rounded-lg ${className}`} {...props}>
+      {children}
+    </div>
+  );
+}
+
+export function CardContent({ children, className = "", ...props }) {
+  return (
+    <div className={className} {...props}>
+      {children}
+    </div>
+  );
+}
